ffc3eb451bb3daea04bcbc11c9f9ce00331d9f41
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-09-03 17:43:41 +0100

    Task3

--- Task3/components/LittleLemonContent.js
+++ Task3/components/LittleLemonContent.js
@@ -5,14 +5,14 @@ export default function LittleLemonContent() {
     'Hummus \n Moutabal \n Falafel \n Marinated Olives \n Kofta \n Eggplant Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramisu \n Panna Cotta',
   ]; 
   return (
-    <View style={{ flex: 0.8 }}>
+    <View style={{ flex: 0.9 }}>
       <ScrollView
         indicatorStyle={"white"}
         horizontal={false}
         style={{
           paddingHorizontal: 40,
           paddingVertical: 40,
-          backgroundColor: "#495E65",
+          backgroundColor: "#495E57",
         }}>
         <Text style={{ color: 'white', fontSize: 40, flexWrap: 'wrap' }}>
           View Menu
