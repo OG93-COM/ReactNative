5385712c6cb2066aebd4371a6d23033ef760fa76
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-09-13 00:24:10 +0100

    Task5

--- Task6 SectionList/App.js
+++ Task6 SectionList/App.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
-
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import MenuItems from './components/MenuItems';
