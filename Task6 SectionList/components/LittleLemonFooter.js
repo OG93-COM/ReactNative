b273f749c9db1eb2ffa2670195d6202429f6a83e
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-09-15 00:35:31 +0100

    Task6

--- Task6 SectionList/components/LittleLemonFooter.js
+++ Task6 SectionList/components/LittleLemonFooter.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+
 export default function LittleLemonFooter() {
   return (
     <View style={styles.container}>
