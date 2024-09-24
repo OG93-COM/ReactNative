3b855fa6cea84630c7f77a43f706402f08ee01ce
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-09-24 20:24:48 +0100

    Navigation

--- Task12 Navigation/App.js
+++ Task12 Navigation/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -10,15 +10,34 @@ import Login from "./Login";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
+  const LogoHeaderIcon = () => (
+    <Image
+        source={require("./assets/logo.png")}
+        resizeMode="contain"
+        style={styles.logo}
+        accessible={true}
+        accessiblityLabel="Logo of Little Lemon"
+      /> 
+      )
   return (
     <>
     <LittleLemonHeader/>
     <NavigationContainer>
       <Stack.Navigator
       initialRouteName="Login"
-      screenOptions={{headerStyle:{backgroundColor:"#EEEEEE"}}}
+      screenOptions={{
+        headerStyle:{backgroundColor:"#333329"},
+        headerTintColor:"#FFFFFF",
+        headerTitleStyle:{
+          fontWeight:"bold",
+          fontSize:15,
+        }
+      }}
       >
-        <Stack.Screen options={{title:'Home'}} name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen 
+        options={{ title:'Home', headerTitle: (props) => <LogoHeaderIcon {...props}/>}}
+          name="Welcome"
+          component={WelcomeScreen} />
         <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -33,4 +52,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#333333",
   },
   footerContainer: { backgroundColor: "#333333" },
+  logo:{
+    height:50,
+    width:50,
+    alignSelf:"center"
+  }
 });
