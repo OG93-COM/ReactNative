712d05c009f7d6cc139f122cd981b9b1eca2cc5e
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-09-25 18:32:04 +0100

    Navigation TAB Exercise

--- Task14 TAB Navigation Exercise/App.js
+++ Task14 TAB Navigation Exercise/App.js
@@ -1,14 +1,17 @@
-import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import * as React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-import LittleLemonHeader from './components/LittleLemonHeader';
-import LittleLemonFooter from './components/LittleLemonFooter';
-import WelcomeScreen from './WelcomeScreen';
-import LoginScreen from './LoginScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LittleLemonHeader from "./components/LittleLemonHeader";
+import LittleLemonFooter from "./components/LittleLemonFooter";
+import WelcomeScreen from "./WelcomeScreen";
+import LoginScreen from "./LoginScreen";
 
-const Stack = createNativeStackNavigator();
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
@@ -16,10 +19,29 @@ export default function App() {
       <NavigationContainer>
         <View style={styles.container}>
           <LittleLemonHeader />
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Welcome" component={WelcomeScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-          </Stack.Navigator>
+          <Tab.Navigator
+            initialRouteName="Login"
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if (route.name === "Welcome") {
+                  iconName = focused
+                    ? "home"
+                    : "home-outline";
+                } else if (route.name === "Login") {
+                  iconName = focused ? "enter" : "enter-outline";
+                }
+
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: "tomato",
+              tabBarInactiveTintColor: "gray",
+            })}
+          >
+            <Tab.Screen name="Welcome" component={WelcomeScreen} />
+            <Tab.Screen name="Login" component={LoginScreen} />
+          </Tab.Navigator>
         </View>
         <View style={styles.footerContainer}>
           <LittleLemonFooter />
@@ -32,8 +54,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333333',
+    backgroundColor: "#333333",
   },
-  footerContainer: { backgroundColor: '#333333' },
+  footerContainer: { backgroundColor: "#333333" },
 });
-
