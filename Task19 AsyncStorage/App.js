b1d5c7bb3faaffbf0863c62a073b22be372a9f81
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-24 00:38:53 +0100

    Async Storage

--- Task19 AsyncStorage/App.js
+++ Task19 AsyncStorage/App.js
@@ -1,23 +1,20 @@
-import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import Constants from "expo-constants";
-import { Switch } from "react-native-paper";
-import { useEffect } from "react";
+import { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Alert } from 'react-native';
+import Constants from 'expo-constants';
+import { Switch } from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import useUpdate from './useUpdate';
 
 export default function App() {
-  const [preferences, setPreferences] = React.useState({
+  const [preferences, setPreferences] = useState({
     pushNotifications: false,
     emailMarketing: false,
     latestNews: false,
   });
 
-  const updateState = (key) => () =>
-    setPreferences((prevState) => ({
-      ...prevState,
-      [key]: !prevState[key],
-    }));
-
   useEffect(() => {
+    // Populating preferences from storage using AsyncStorage.multiGet
     (async () => {
       try {
         const values = await AsyncStorage.multiGet(Object.keys(preferences));
@@ -33,29 +30,50 @@ export default function App() {
     })();
   }, []);
 
+  // This effect only runs when the preferences state updates, excluding initial mount
+  useUpdate(() => {
+    (async () => {
+      // Every time there is an update on the preference state, we persist it on storage
+      // The exercise requierement is to use multiSet API
+      const keyValues = Object.entries(preferences).map((entry) => {
+        return [entry[0], String(entry[1])];
+      });
+      try {
+        await AsyncStorage.multiSet(keyValues);
+      } catch (e) {
+        Alert.alert(`An error occurred: ${e.message}`);
+      }
+    })();
+  }, [preferences]);
+
+  const updateState = (key) => () =>
+    setPreferences((prevState) => ({
+      ...prevState,
+      [key]: !prevState[key],
+    }));
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Account Preferences</Text>
       <View style={styles.row}>
-        <Text>Push notifications</Text>
+        <Text style={styles.text}>Push notifications</Text>
         <Switch
           value={preferences.pushNotifications}
-          onValueChange={updateState("pushNotifications")}
+          onValueChange={updateState('pushNotifications')}
         />
       </View>
       <View style={styles.row}>
-        <Text>Marketing emails</Text>
+        <Text style={styles.text}>Marketing emails</Text>
         <Switch
           value={preferences.emailMarketing}
-          onValueChange={updateState("emailMarketing")}
+          onValueChange={updateState('emailMarketing')}
         />
       </View>
       <View style={styles.row}>
-        <Text>Latest news</Text>
+        <Text style={styles.text}>Latest news</Text>
         <Switch
           value={preferences.latestNews}
-          onValueChange={updateState("latestNews")}
+          onValueChange={updateState('latestNews')}
         />
       </View>
     </View>
@@ -66,18 +84,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: "#ecf0f1",
+    backgroundColor: '#ecf0f1',
     padding: 16,
   },
   row: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     paddingVertical: 16,
+  },
+  text: {
+    fontSize: 18,
   },
   header: {
     margin: 24,
     fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
